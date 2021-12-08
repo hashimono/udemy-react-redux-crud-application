5d94d63b926ad9f35fd54cbbf1b0dbd5bcce788d
hashimono <onoeono@gmail.com>
2021-12-08 21:59:50 +0900

    plus jsx

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import React, { Component } from 'react';
 
 class App extends Component {
   render() {
-    return <h1>Hello, world!</h1>
-  }
+    return (
+      <React.Fragment>
+        <label htmlFor="bar">bar</label>
+        <input type="button" onClick={() => console.log("I am cliked.")} value="BUTTON" />
+      </React.Fragment>
+    )
+   }
 }
 export default App;
