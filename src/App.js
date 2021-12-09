e17a3409ab325227a224a7209471d1270885ea15
hashimono <onoeono@gmail.com>
2021-12-09 21:48:08 +0900

    props.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <label htmlFor="bar">bar</label>
-        <input type="button" onClick={() => console.log("I am cliked.")} value="BUTTON" />
-      </React.Fragment>
-    )
-   }
+const App = () => {
+  const profiles = [
+    {name: "Taro", age: 12}
+    , {name: "Hanako", age: 8}
+    , {name: "NoAge"}
+  ]
+  return profiles.map((profile, index) => {
+    return <User name={profile.name} age={profile.age} key={index} />
+  });
 }
+
+
+const User = (props) => {
+  return <div>Hi, I am {props.name}. {props.age} years old.</div>
+}
+
+User.defaultProps = {
+  age: 1
+}
+
 export default App;
